251753efc054cfa7af5240eb6dd74faea7f6b060
Kadir Gün <kadir@kadirgun.com>
2024-04-07 08:09:10 +0300

    Add scale option to set device scale factor

--- index.ts
+++ index.ts
@@ -14,16 +14,17 @@ export type TableData = {
 export type Options = {
   width?: number;
   height?: number;
+  scale?: number;
 };
 
 export async function getTableImage(data: TableData, options: Options = {}): Promise<Buffer> {
   const url = `https://table-generator.kadirgun.com/#${btoa(JSON.stringify(data))}`;
 
   const browser = await puppeteer.launch({
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    args: ["--no-sandbox", "--disable-setuid-sandbox", `--force-device-scale-factor=${options.scale || 1}`],
   });
   const page = await browser.newPage();
-  page.setViewport({ width: options.width || 800, height: options.height || 600 });
+  await page.setViewport({ width: options.width || 800, height: options.height || 600 });
   await page.goto(url, { waitUntil: "networkidle0" });
   const image = await page.screenshot({ fullPage: true });
   await browser.close();
